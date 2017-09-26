9a996e97d4e99e12f08c742a2583dbecf2ada245
tearth <Tearth@users.noreply.github.com>
2017-09-27 00:46:33 +0200

    Change structure of GUI project

--- Proximab/GUI/Program.cs
+++ Proximab/GUI/Program.cs
@@ -13,7 +13,7 @@ namespace GUI
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new GUICore())
                 game.Run();
         }
     }
---
+++ Proximab/GUI/Source/GUICore.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GUI
+{
+    public class GUICore : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        
+        public GUICore()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Assets";
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+        
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+        
+        protected override void UnloadContent()
+        {
+
+        }
+        
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
