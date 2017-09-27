17c771402cde428672545e346419cd4863f9bebe
tearth <Tearth@users.noreply.github.com>
2017-09-27 22:54:39 +0200

    Remove built-in content manager

---
+++ Proximab/GUI/Source/ConsoleSubsystem/CommandDefinitions/DefinitionsManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.Source.ConsoleSubsystem.CommandDefinitions
+{
+    internal class DefinitionsManager
+    {
+        public DefinitionsManager()
+        {
+           // List
+        }
+
+        public void Load()
+        {
+
+        }
+    }
+}
--- Proximab/GUI/Source/GUICore.cs
+++ Proximab/GUI/Source/GUICore.cs
@@ -1,7 +1,9 @@
-﻿using GUI.Source.ConsoleSubsystem;
+﻿using ContentDefinitions;
+using GUI.Source.ConsoleSubsystem;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace GUI
 {
