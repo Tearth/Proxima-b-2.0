9979b2b6cf436df361863e860e7502503038907b
tearth <Tearth@users.noreply.github.com>
2017-09-29 16:36:53 +0200

    Add Core project with PieceType class

---
+++ Proximab/Core/Board/PieceType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Board
+{
+    public enum PieceType : int
+    {
+        None = 0,
+
+        WhitePawn = 1,
+        WhiteRook = 2,
+        WhiteKnight = 3,
+        WhiteBishop = 4,
+        WhiteQueen = 5,
+        WhiteKing = 6,
+
+        BlackPawn = -1,
+        BlackRook = -2,
+        BlackKnight = -3,
+        BlackBishop = -4,
+        BlackQueen = -5,
+        BlackKing = -6
+    }
+}
