e00f047765d4cd9bb39cd69119510fb19b91867a
tearth <Tearth@users.noreply.github.com>
2017-10-06 01:00:57 +0200

    Add Color enum

---
+++ Proximab/Core/Commons/Color.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Commons
+{
+    public enum Color
+    {
+        None = 0,
+        White = 1,
+        Black = 2
+    }
+}
