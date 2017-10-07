3b6f23066d6291b951a8a6dc2802d0bbdc8bbd5a
tearth <Tearth@users.noreply.github.com>
2017-10-07 15:10:50 +0200

    Add predefined king moves

---
+++ Proximab/Core/Boards/MoveGenerators/KingMovesGenerator.cs
@@ -0,0 +1,36 @@
+﻿namespace Core.Boards.MoveGenerators
+{
+    public class KingMovesGenerator
+    {
+        public KingMovesGenerator()
+        {
+
+        }
+
+        public ulong[] Generate()
+        {
+            var predefinedMoves = new ulong[64];
+            var bitPosition = 1ul;
+
+            for (int i = 0; i < 64; i++)
+            {
+                predefinedMoves[i] = GetPattern(bitPosition);
+                bitPosition = bitPosition << 1;
+            }
+
+            return predefinedMoves;
+        }
+
+        ulong GetPattern(ulong field)
+        {
+            return ((field & ~BitConstants.ALine) << 1) |
+                   ((field & ~BitConstants.HLine) << 7) |
+                   ( field                        << 8) |
+                   ((field & ~BitConstants.ALine) << 9) |
+                   ((field & ~BitConstants.HLine) >> 1) |
+                   ((field & ~BitConstants.ALine) >> 7) |
+                    (field                        >> 8) |
+                   ((field & ~BitConstants.HLine) >> 9);
+        }
+    }
+}
--- Proximab/Core/Boards/MoveGenerators/PredefinedMoves.cs
+++ Proximab/Core/Boards/MoveGenerators/PredefinedMoves.cs
@@ -3,6 +3,7 @@
     public static class PredefinedMoves
     {
         public static ulong[] Knight { get; private set; }
+        public static ulong[] King { get; private set; }
 
         static bool Ready = false;
 
@@ -12,8 +13,10 @@
                 return;
 
             var knightMovesGenerator = new KnightMovesGenerator();
+            var kingMovesGenerator = new KingMovesGenerator();
 
             Knight = knightMovesGenerator.Generate();
+            King = kingMovesGenerator.Generate();
 
             Ready = true;
         }
