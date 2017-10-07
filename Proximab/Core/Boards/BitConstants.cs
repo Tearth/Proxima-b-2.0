d5752977165724e54f2f896b35c27e90fc4ed3b2
tearth <Tearth@users.noreply.github.com>
2017-10-07 15:32:58 +0200

    Add constants for board ranks

--- Proximab/Core/Boards/BitConstants.cs
+++ Proximab/Core/Boards/BitConstants.cs
@@ -10,5 +10,14 @@
         public const ulong FFile = 0x0404040404040404ul;
         public const ulong GFile = 0x0202020202020202ul;
         public const ulong HFile = 0x0101010101010101ul;
+
+        public const ulong ARank = 0x00000000000000FFul;
+        public const ulong BRank = 0x000000000000FF00ul;
+        public const ulong CRank = 0x0000000000FF0000ul;
+        public const ulong DRank = 0x00000000FF000000ul;
+        public const ulong ERank = 0x000000FF00000000ul;
+        public const ulong FRank = 0x0000FF0000000000ul;
+        public const ulong GRank = 0x00FF000000000000ul;
+        public const ulong HRank = 0xFF00000000000000ul;
     }
 }
