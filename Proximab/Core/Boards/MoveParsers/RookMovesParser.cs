c2092631996ba10eabfcde565faea0913f8e340f
tearth <Tearth@users.noreply.github.com>
2017-10-09 13:18:53 +0200

    Add X-Ray attack for rook

--- Proximab/Core/Boards/MoveParsers/RookMovesParser.cs
+++ Proximab/Core/Boards/MoveParsers/RookMovesParser.cs
@@ -25,7 +25,7 @@ namespace Core.Boards.MoveParsers
             var pieces = _bitBoard.Pieces[(int)color, (int)PieceType.Rook];
             var moves = CalculateMoves(pieces, color, friendlyOccupancy, enemyOccupancy);
 
-            CalculateXRay(pieces, color, friendlyOccupancy, enemyOccupancy);
+            CalculateAttackFields(pieces, color, friendlyOccupancy, enemyOccupancy);
 
             return moves;
         }
@@ -41,8 +41,8 @@ namespace Core.Boards.MoveParsers
                 var pieceIndex = BitOperations.GetBitIndex(pieceLSB);
                 var piecePosition = BitPositionConverter.ToPosition(pieceLSB);
 
-                var horizontalPattern = GetHorizontalPattern(occupancy, piecePosition);
-                var verticalPattern = GetVerticalPattern(occupancy, piecePosition);
+                var horizontalPattern = GetHorizontalPattern(occupancy, piecePosition, false);
+                var verticalPattern = GetVerticalPattern(occupancy, piecePosition, false);
 
                 var pattern = (horizontalPattern | verticalPattern) & ~friendlyOccupancy;
 
@@ -56,15 +56,13 @@ namespace Core.Boards.MoveParsers
                     var moveType = GetMoveType(patternLSB, enemyOccupancy);
 
                     moves.Add(new Move(from, to, PieceType.Rook, color, moveType));
-
-                    _bitBoard.Attacks[(int)color, patternIndex] |= pieceLSB;
                 }
             }
 
             return moves;
         }
 
-        void CalculateXRay(ulong pieces, Color color, ulong friendlyOccupancy, ulong enemyOccupancy)
+        void CalculateAttackFields(ulong pieces, Color color, ulong friendlyOccupancy, ulong enemyOccupancy)
         {
             var blockersToRemove = _bitBoard.Pieces[(int)color, (int)PieceType.Rook] |
                                    _bitBoard.Pieces[(int)color, (int)PieceType.Queen];
@@ -77,10 +75,13 @@ namespace Core.Boards.MoveParsers
                 var pieceIndex = BitOperations.GetBitIndex(pieceLSB);
                 var piecePosition = BitPositionConverter.ToPosition(pieceLSB);
 
-                var horizontalPattern = GetHorizontalPattern(occupancy, piecePosition);
-                var verticalPattern = GetVerticalPattern(occupancy, piecePosition);
+                var horizontalPattern = GetHorizontalPattern(occupancy, piecePosition, true);
+                var verticalPattern = GetVerticalPattern(occupancy, piecePosition, true);
 
-                var pattern = (horizontalPattern | verticalPattern) & ~friendlyOccupancy;
+                horizontalPattern = ExpandPatternByFriendlyPieces(horizontalPattern, friendlyOccupancy, Axis.Rank, color);
+                verticalPattern = ExpandPatternByFriendlyPieces(verticalPattern, friendlyOccupancy, Axis.File, color);
+
+                var pattern = horizontalPattern | verticalPattern;
 
                 while (pattern != 0)
                 {
@@ -92,7 +93,7 @@ namespace Core.Boards.MoveParsers
             }
         }
 
-        ulong GetHorizontalPattern(ulong occupancy, Position piecePosition)
+        ulong GetHorizontalPattern(ulong occupancy, Position piecePosition, bool includeFriendlyBlockerMoves)
         {
             var offset = piecePosition.Y - 1;
 
@@ -102,15 +103,43 @@ namespace Core.Boards.MoveParsers
             return (ulong)pattern << (offset * 8);
         }
 
-        ulong GetVerticalPattern(ulong occupancy, Position piecePosition)
+        ulong GetVerticalPattern(ulong occupancy, Position piecePosition, bool includeFriendlyBlockerMoves)
         {
             var offset = 8 - piecePosition.X;
             var rotatedOccupancy = BitOperations.RotateRight(occupancy);
 
             var pieceRank = (byte)(rotatedOccupancy >> (offset * 8));
             var pattern = PredefinedMoves.Rook[pieceRank, 8 - piecePosition.Y];
-            
+
             return BitOperations.RotateLeft(pattern) << offset;
+        }
+
+        ulong ExpandPatternByFriendlyPieces(ulong pattern, ulong friendlyOccupation, Axis axis, Color color)
+        {
+            var expandedPattern = pattern;
+
+            var blockers = pattern & friendlyOccupation;
+            var patternLSB = BitOperations.GetLSB(ref pattern);
+
+            var shift = axis == Axis.File ? 8 : 1;
+
+            while(blockers != 0)
+            {
+                var blockerLSB = BitOperations.GetLSB(ref blockers);
+                if ((blockerLSB & _bitBoard.Pieces[(int)color, (int)PieceType.King]) != 0)
+                {
+                    if(blockerLSB == patternLSB)
+                    {
+                        expandedPattern |= blockerLSB >> shift;
+                    }
+                    else
+                    {
+                        expandedPattern |= blockerLSB << shift;
+                    }
+                }
+            }
+
+            return expandedPattern;
         }
     }
 }
