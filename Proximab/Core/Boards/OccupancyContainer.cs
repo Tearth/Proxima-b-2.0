bd13385a53bc87c3272a72874162528bb7142d82
tearth <Tearth@users.noreply.github.com>
2017-10-12 00:11:36 +0200

    Add OccupancyContainer class

---
+++ Proximab/Core/Boards/OccupancyContainer.cs
@@ -0,0 +1,29 @@
+﻿using Core.Commons.Colors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Boards
+{
+    public class OccupancyContainer
+    {
+        public ulong FriendlyOccupancy { get; set; }
+        public ulong EnemyOccupancy { get; set; }
+        public ulong Occupancy { get; set; }
+
+        public OccupancyContainer()
+        {
+
+        }
+
+        public OccupancyContainer(Color color, ulong[] occupancy)
+        {
+            FriendlyOccupancy = occupancy[(int)color];
+            EnemyOccupancy = occupancy[(int)ColorOperations.Invert(color)];
+
+            Occupancy = FriendlyOccupancy | Occupancy;
+        }
+    }
+}
