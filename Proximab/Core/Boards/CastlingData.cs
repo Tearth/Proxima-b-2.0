7931c6e87b98a9f35f00c03d4dc8489070f45c60
tearth <Tearth@users.noreply.github.com>
2017-10-14 17:20:54 +0200

    Add CastlingData class

---
+++ Proximab/Core/Boards/CastlingData.cs
@@ -0,0 +1,20 @@
+﻿namespace Core.Boards
+{
+    public class CastlingData
+    {
+        public bool WhiteShortCastlingPossible { get; set; }
+        public bool WhiteLongCastlingPossible { get; set; }
+
+        public bool BlackShortCastlingPossible { get; set; }
+        public bool BlackLongCastlingPossible { get; set; }
+
+        public CastlingData()
+        {
+            WhiteShortCastlingPossible = true;
+            WhiteLongCastlingPossible = true;
+
+            BlackShortCastlingPossible = true;
+            BlackLongCastlingPossible = true;
+        }
+    }
+}
