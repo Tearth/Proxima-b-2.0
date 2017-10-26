88c81fa8d421f1d521ac95ff6d94ddf7777ac9cb
tearth <Tearth@users.noreply.github.com>
2017-10-26 20:17:15 +0200

    Add FriendlyAttack class

---
+++ Proximab/Proxima.Core/Boards/Friendly/FriendlyAttack.cs
@@ -0,0 +1,27 @@
+﻿using Proxima.Core.Commons.Colors;
+using Proxima.Core.Commons.Positions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxima.Core.Boards.Friendly
+{
+    public class FriendlyAttack
+    {
+        public Color Color { get; set; }
+        public Position To { get; set; }
+
+        public FriendlyAttack()
+        {
+
+        }
+
+        public FriendlyAttack(Color color, Position to)
+        {
+            Color = Color;
+            To = to;
+        }
+    }
+}
