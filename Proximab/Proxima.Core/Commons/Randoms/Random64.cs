6ebd7a9d63b0e835a593cac7aefe5688ef6f636d
tearth <Tearth@users.noreply.github.com>
2017-11-06 16:58:25 +0100

    Add Random64 class

---
+++ Proximab/Proxima.Core/Commons/Randoms/Random64.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Proxima.Core.Commons.Randoms
+{
+    public class Random64
+    {
+        Random _random;
+
+        public Random64()
+        {
+            _random = new Random();
+        }
+
+        public ulong Next()
+        {
+            var leftPart = (ulong)_random.Next() << 32;
+            var rightPart = (ulong)_random.Next();
+
+            return leftPart | rightPart;
+        }
+    }
+}
