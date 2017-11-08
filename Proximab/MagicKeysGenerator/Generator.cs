7a5de19719a05393345fc515df10c8aef12927d2
tearth <Tearth@users.noreply.github.com>
2017-11-08 01:09:54 +0100

    Add MagicKeysGenerator project

---
+++ Proximab/MagicKeysGenerator/Generator.cs
@@ -0,0 +1,42 @@
+﻿using Proxima.Core;
+using Proxima.Core.Boards;
+using Proxima.Core.MoveGenerators.MagicBitboards.Generators;
+using Proxima.Core.MoveGenerators.PatternGenerators;
+using System;
+
+namespace MagicKeysGenerator
+{
+    internal class Generator
+    {
+        MagicKeyGenerator _magicKeyGenerator;
+        RookAttacksGenerator _rookAttacksGenerator;
+
+        public Generator()
+        {
+            _magicKeyGenerator = new MagicKeyGenerator();
+            _rookAttacksGenerator = new RookAttacksGenerator();
+
+            ProximaCore.Init();
+        }
+
+        public ulong[] GetKeys()
+        {
+            var keys = new ulong[64];
+
+            for(int i=0; i<64; i++)
+            {
+                var mask = PatternsContainer.RookPattern[i];
+                var maskSize = BitOperations.Count(mask);
+                var patterns = _rookAttacksGenerator.Generate(i);
+
+                keys[i] = _magicKeyGenerator.GenerateKey(patterns, mask);
+
+                Console.WriteLine($"Key {i} = {keys[i]}, " +
+                                  $"mask size: {maskSize}, " +
+                                  $"patterns size: {patterns.Count}");
+            }
+
+            return keys;
+        }
+    }
+}
---
+++ Proximab/MagicKeysGenerator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MagicKeysGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting magic keys generator...");
+            Console.WriteLine();
+
+            var generator = new Generator();
+            var keys = generator.GetKeys();
+
+            Console.WriteLine();
+            Console.WriteLine("Operation completed!");
+            Console.ReadLine();
+        }
+    }
+}
