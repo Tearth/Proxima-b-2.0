995d1874c8d22ad9086b52e9d8e26cce8c0f154f
tearth <Tearth@users.noreply.github.com>
2017-11-10 17:32:29 +0100

    Add InvalidMagicKeyException class

---
+++ Proximab/Proxima.Core/MoveGenerators/MagicBitboards/Exceptions/InvalidMagicKeyException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Proxima.Core.MoveGenerators.MagicBitboards.Exceptions
+{
+    class InvalidMagicKeyException : Exception
+    {
+        public InvalidMagicKeyException() : base()
+        {
+
+        }
+
+        public InvalidMagicKeyException(string message) : base(message)
+        {
+
+        }
+
+        public InvalidMagicKeyException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+
+        public InvalidMagicKeyException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+
+        }
+    }
+}
