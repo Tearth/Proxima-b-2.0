3dcf65f32b5e01bd2e8559cd9994aebf22d649f1
tearth <Tearth@users.noreply.github.com>
2017-12-13 11:56:01 +0100

    Add Style12 container

---
+++ Proximab/Proxima.FICS/Source/GameSubsystem/Modes/Game/Style12/Style12Container.cs
@@ -0,0 +1,119 @@
+﻿namespace Proxima.FICS.Source.GameSubsystem.Modes.Game.Style12
+{
+    /// <summary>
+    /// Represents a container for Style12 FICS response.
+    /// </summary>
+    public class Style12Container
+    {
+        /// <summary>
+        /// Gets or sets the style id (will be always 12).
+        /// </summary>
+        public string StyleID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the current board state (each row means separate rank). First one is White's 8th rank, then 7th rank etc.
+        /// </summary>
+        public string[] BoardState { get; set; }
+
+        /// <summary>
+        /// Gets or sets the color of current player whose turn it is.
+        /// </summary>
+        public char ColorToMove { get; set; }
+
+        /// <summary>
+        /// Gets or sets the double pawn push file (-1 if none).
+        /// </summary>
+        public int DoublePawnPush { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the white short castling is possible.
+        /// </summary>
+        public bool WhiteShortCastlingPossible { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the white long castling is possible.
+        /// </summary>
+        public bool WhiteLongCastlingPossible { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the black short castling is possible.
+        /// </summary>
+        public bool BlackShortCastlingPossible { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the black long castling is possible.
+        /// </summary>
+        public bool BlackLongCastlingPossible { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of moves made since last irreversible move.
+        /// </summary>
+        public int Rule50Moves { get; set; }
+
+        /// <summary>
+        /// Gets or sets the game number (assigned by FICS).
+        /// </summary>
+        public int GameNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the white player name.
+        /// </summary>
+        public string WhitePlayerName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the black player name.
+        /// </summary>
+        public string BlackPlayerName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the relation of engine to the game (-3 = isolated position, -2 = examiner, 
+        /// -1 = move of the enemy, 1 = move of the engine, 0 = observing).
+        /// </summary>
+        public int Relation { get; set; }
+
+        /// <summary>
+        /// Gets or sets the initial time (in seconds).
+        /// </summary>
+        public int InitialTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the incremental time (in seconds).
+        /// </summary>
+        public int IncrementalTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the remaining time of white player.
+        /// </summary>
+        public int WhiteRemainingTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the remaining time of black player.
+        /// </summary>
+        public int BlackRemainingTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of moves to made (will be always 1).
+        /// </summary>
+        public int MovesToMade { get; set; }
+
+        /// <summary>
+        /// Gets or sets the verbose previous move notation (e2-e4, h8-b8 etc). None if this is the first move.
+        /// </summary>
+        public string VerbosePreviousMoveNotation { get; set; }
+
+        /// <summary>
+        /// Gets or sets the time taken to do the previous move.
+        /// </summary>
+        public string TimeOfPreviousMove { get; set; }
+
+        /// <summary>
+        /// Gets or sets the pretty previous move notation (e4, Nc6 etc). None if this is the first move.
+        /// </summary>
+        public string PrettyPreviousMoveNotation { get; set; }
+
+        /// <summary>
+        /// Gets or sets the board orientation (0 = white at bottom, 1 = black at bottom).
+        /// </summary>
+        public int BoardOrientation { get; set; }
+    }
+}
