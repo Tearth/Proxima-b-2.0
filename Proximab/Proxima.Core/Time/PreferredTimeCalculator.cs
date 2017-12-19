74852eb608a52679359620a2846a67f37c0e7c71
tearth <Tearth@users.noreply.github.com>
2017-12-19 23:19:11 +0100

    Add PreferredTimeCalculator class

---
+++ Proximab/Proxima.Core/Time/PreferredTimeCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxima.Core.Time
+{
+    public class PreferredTimeCalculator
+    {
+        private int _edge;
+
+        public PreferredTimeCalculator(int edge)
+        {
+            _edge = edge;
+        }
+
+        public float Calculate(int movesCount, int remainingTime)
+        {
+            if(movesCount < _edge)
+            {
+                return (float)remainingTime / movesCount;
+            }
+            else
+            {
+                return (float)remainingTime / _edge;
+            }
+        }
+    }
+}
