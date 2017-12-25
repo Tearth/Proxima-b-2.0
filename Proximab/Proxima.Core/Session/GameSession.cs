5663a80eb8ea9c4a3e0a8ff4d0da20a8aeb6cd3a
tearth <Tearth@users.noreply.github.com>
2017-12-25 13:01:13 +0100

    Add GameSession class

---
+++ Proximab/Proxima.Core/Session/GameSession.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Proxima.Core.AI;
+using Proxima.Core.Boards;
+using Proxima.Core.Boards.Friendly;
+
+namespace Proxima.Core.Session
+{
+    public class GameSession
+    {
+        public int MovesCount { get; private set; }
+
+        private AICore _aiCore;
+        private Bitboard _bitboard;
+
+        public GameSession()
+        {
+            MovesCount = 0;
+
+            _aiCore = new AICore();
+            _bitboard = new Bitboard(new DefaultFriendlyBoard());
+        }
+    }
+}
