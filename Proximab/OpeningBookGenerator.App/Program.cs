5a96e45e932dde123af78cfd91d1666ec6e5f343
tearth <Tearth@users.noreply.github.com>
2018-01-03 20:10:23 +0100

    Add method to save the opening book

--- Proximab/OpeningBookGenerator.App/Program.cs
+++ Proximab/OpeningBookGenerator.App/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Proxima.Core.MoveGenerators.Moves;
 
 namespace OpeningBookGenerator.App
 {
@@ -16,6 +18,28 @@ namespace OpeningBookGenerator.App
             Console.WriteLine();
 
             var openingBook = generator.GetOpeningBook("Books/input.book");
+            SaveOpeningBook("Books/output.book", openingBook);
+        }
+
+        private static void SaveOpeningBook(string outputFile, List<List<Move>> openingBook)
+        {
+            using (var streamWriter = new StreamWriter(outputFile))
+            {
+                foreach (var opening in openingBook)
+                {
+                    for(var i=0; i<opening.Count; i++)
+                    {
+                        if (i > 0)
+                        {
+                            streamWriter.Write(" ");
+                        }
+
+                        streamWriter.Write(opening[i].ToString());
+                    }
+
+                    streamWriter.WriteLine();
+                }
+            }
         }
     }
 }
